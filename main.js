a6cfbb65e3f123b5f646ae94514d0a491ea1f67e
Abhishek Pal <abhipal2330@gmail.com>
2025-08-09 14:59:37 +0530

    updated

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
-<<<<<<< HEAD
+function raf() {
+    requestAnimationFrame(raf);
+}
 
+requestAnimationFrame(raf);
 
-requestAnimationFrame(raf)  
-        // Redirect to index.html on page refresh
-        window.addEventListener("beforeunload", function () {
-            window.location.href = "index.html";
-        });
-    
+window.addEventListener("beforeunload", function () {
+    window.location.href = "index.html";
+});
 
-
-
-//video wala part 
-
-
-const videos = document.querySelectorAll("#content video"); 
+const videos = document.querySelectorAll("#content video");
 
 videos.forEach(video => {
     video.addEventListener('timeupdate', function () {
@@ -23,35 +18,3 @@ videos.forEach(video => {
         }
     });
 });
-
-
-
-=======
-
-
-requestAnimationFrame(raf)  
-        // Redirect to index.html on page refresh
-        window.addEventListener("beforeunload", function () {
-            window.location.href = "index.html";
-        });
-    
-
-
-
-//video wala part 
-
-
-const videos = document.querySelectorAll("#content video"); 
-
-videos.forEach(video => {
-    video.addEventListener('timeupdate', function () {
-        if (video.currentTime >= video.duration / 2) {
-            video.currentTime = 0;
-            video.play();
-        }
-    });
-});
-
-
-
->>>>>>> 767f567a4cf9d36e9c4c5653b66c17496c088d8f
--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
-
-
-
 var tl = gsap.timeline();
+
 tl.to(".loading", {
     opacity: 0,
     delay: 2.5,
-    duration: 1
- })
+    duration: 1 
+})
 .to(".loader", {
     opacity: 0,
     duration: 1,
@@ -34,8 +31,6 @@ tl.to(".loading", {
     rotationZ: "+=15"
 }, "up");
 
-
-
 var tl2 = gsap.timeline();
 
 document.querySelectorAll("#explore h1").forEach(function (elem) {
@@ -45,87 +40,17 @@ document.querySelectorAll("#explore h1").forEach(function (elem) {
             width: "80vw",
             top: "-10%",
             rotate: "180deg",
-            ease: "power3.out",  
-            duration: 1.5,      
+            ease: "power3.out",
+            duration: 1.5,
             opacity: 0,
-            delay: 0.2          
+            delay: 0.2
         })
-            .to(".svgwaala", {
-                opacity: 0
-            });
-            
+        .to(".svgwaala", {
+            opacity: 0
+        });
     });
 });
 
-
 function redirectToMain() {
-    window.location.href = "main.html"; 
+    window.location.href = "main.html";
 }
-
-
-=======
-
-
-
-var tl = gsap.timeline();
-tl.to(".loading", {
-    opacity: 0,
-    delay: 2.5,
-    duration: 1
- })
-.to(".loader", {
-    opacity: 0,
-    duration: 1,
-    ease: "Expo.easeInOut"
-})
-.to(".loader", {
-    y: "-100%",
-    duration: 1,
-    ease: "Expo.easeInOut"
-}, "up")
-.to(".svgwaala", {
-    opacity: 1,
-    delay: -1.7,
-    ease: "Expo.easeInOut"
-}, "donut")
-.to("#shoe", {
-    opacity: 1,
-    scale: 2.2,
-    delay: -1.4,
-    ease: "Expo.easeInOut",
-    rotationY: "+=15",
-    duration: 3,
-    rotationX: "+=15",
-    rotationZ: "+=15"
-}, "up");
-
-
-
-var tl2 = gsap.timeline();
-
-document.querySelectorAll("#explore h1").forEach(function (elem) {
-    elem.addEventListener("click", function () {
-        tl2
-        .to("#shoe", {
-            width: "80vw",
-            top: "-10%",
-            rotate: "180deg",
-            ease: "power3.out",  
-            duration: 1.5,      
-            opacity: 0,
-            delay: 0.2          
-        })
-            .to(".svgwaala", {
-                opacity: 0
-            });
-            
-    });
-});
-
-
-function redirectToMain() {
-    window.location.href = "main.html"; 
-}
-
-
->>>>>>> 767f567a4cf9d36e9c4c5653b66c17496c088d8f
